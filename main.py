8b5bcaf7afbe7529f026e08d6b0aaf7e9606fdfa
pafimov <pafimov@yandex.ru>
2023-04-26 21:10:36 +0300

    Multiple maps added

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from aiogram import Bot, executor, Dispatcher, types
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import sqlite3 as sq
+import emoji
 
+
+storage = MemoryStorage()
 bot = Bot('6235349333:AAH37djTzR62bJYXgDnbPKdA8K0wo-lktBU')
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
 def start_db():
     global base, cur
@@ -12,6 +17,7 @@ def start_db():
     if base:
         print("DB OKAY")
     base.execute('CREATE TABLE IF NOT EXISTS users(id BIGINT PRIMARY KEY, game BIGINT)')
+    base.execute('UPDATE users SET game=?', ('-1',))
     base.commit()
     
 def add_to_db(id):
@@ -44,81 +50,91 @@ async def go_hello(message : types.Message):
         await message.reply('Вы сейчас находитесь в игре. Продолжите игру, либо, если хотите её завершить, нажмите /stop ')
 
 
+def check_auth(user_id):
+    if get_user_game(user_id) == -2:
+        print("NEW USER!!")
+        add_to_db(user_id)
 
 class game:
-    symb = ['X', 'O']
-    def __init__(self, _player1, _player2):
+    symb = [emoji.emojize(":cross_mark:"), emoji.emojize(":green_circle:")]
+    neutral = emoji.emojize(":white_large_square:")
+    def __init__(self, _player1, _player2, _sz):
         self.players = [_player1, _player2]
         self.field = []
+        self.sz = _sz
         self.current = 0
-        for i in range(3):
-            row = ['@', '@', '@']
+        self.need_to_win = 3
+        if self.sz >= 5:
+            self.need_to_win = 4
+        for i in range(self.sz):
+            row = [self.neutral for j in range(self.sz)]
             self.field.append(row)
 
+    def check_cell(self, x, y):
+        if x >= self.sz or y >= self.sz or x < 0 or y < 0:
+            return 0
+        if self.field[x][y] == self.neutral:
+            return 0
+        return 1 
+
+    def have_winning_path(self, x, y, dx, dy):
+        cur_el = self.field[x][y]
+        ok = 1
+        for k in range(self.need_to_win):
+            if self.check_cell(x, y) == 0:
+                ok = 0
+                break
+            if self.field[x][y] != cur_el:
+                ok = 0
+                break
+            x += dx
+            y += dy
+        return ok
+
+    #returns -1 if game continues
+    #returns 0 if someone won
+    #returns 1 if its draw
     def check_won(self):
-        #check for the draw
+        #check the draw
         cnt_free = 0
-        for i in range(3):
-            for j in range(3):
-                if self.field[i][j] == '@':
+        for i in range(self.sz):
+            for j in range(self.sz):
+                if self.field[i][j] == self.neutral:
                     cnt_free+=1
         if cnt_free == 0:
             return 1
-        #check vertical
-        for i in range(3):
-            cur_el = self.field[0][i]
-            if cur_el == '@':
-                continue
-            ok = 1
-            for j in range(3):
-                if self.field[j][i] != cur_el:
-                    ok = 0
-            if not ok:
-                continue
-            return 0
-        #check horisontal
-        for i in range(3):
-            cur_el = self.field[i][0]
-            if cur_el == '@':
-                continue
-            ok = 1
-            for j in range(3):
-                if self.field[i][j] != cur_el:
-                    ok = 0
-            if not ok:
-                continue
-            return 0
-        #check main diagonal
-        ok = 1
-        cur_el = self.field[0][0]
-        for i in range(3):
-            if self.field[i][i] != cur_el:
-                ok = 0
-        if ok == 1 and cur_el != '@':
-            return 0
         
-        ok = 1
-        cur_el = self.field[2][0]
-        for i in range(3):
-            if self.field[2-i][i] != cur_el:
-                ok = 0
-        if ok == 1 and cur_el != '@':
-            return 0
+        n = self.sz
+        for i in range(n):
+            for j in range(n):
+                #(i, j) - left upper corner
+                #check bottom way
+                if self.have_winning_path(i, j, 1, 0):
+                    return 0
+                #check right way
+                if self.have_winning_path(i, j, 0, 1):
+                    return 0
+                #check bottom-right way
+                if self.have_winning_path(i, j, 1, 1):
+                    return 0
+                #check bottom-left way
+                if self.have_winning_path(i, j, 1, -1):
+                    return 0
         
         return -1
 
     def get_kb(self):
         kb = InlineKeyboardMarkup(row_width=3)
-        for i in range(3):
+        for i in range(self.sz):
             cur_row = []
-            for j in range(3):
+            for j in range(self.sz):
                 cur_row.append(InlineKeyboardButton(text=self.field[i][j], callback_data=str(i) + ' '+ str(j)))
             kb.row(*cur_row)
         return kb
 
     def go(self, x, y):
         was = self.current
-        if self.field[x][y] != '@':
+        if self.field[x][y] != self.neutral:
             return -1
         
         self.field[x][y] = self.symb[was]
@@ -129,7 +145,7 @@ class game:
         return 2
 
         
-waiting = -1
+waiting = [-1 for i in range(10)]
 free_id = 0
 games = dict()
 
@@ -140,39 +156,61 @@ async def send_field(game_id, final = 0):
         await bot.send_message(players[games[game_id].current], 'Ваш ход - - - - - - - -', reply_markup=kb)
         await bot.send_message(players[1-games[game_id].current], 'Ход соперника- - - -', reply_markup=kb)
     else:
-        await bot.send_message(players[games[game_id].current], 'Финальная карта.', reply_markup=kb)
-        await bot.send_message(players[1-games[game_id].current], 'Финальная карта.', reply_markup=kb)
+        await bot.send_message(players[games[game_id].current], 'Финальная карта- - -', reply_markup=kb)
+        await bot.send_message(players[1-games[game_id].current], 'Финальная карта- - -', reply_markup=kb)
 
+class FSM_search_game(StatesGroup):
+    sz = State()
 
-async def start_game(player1, player2):
+async def start_game(player1, player2, sz):
     global free_id
     edit_user_game(player1, free_id)
     edit_user_game(player2, free_id)
-    new_game = game(player1, player2)
+    new_game = game(player1, player2, sz)
     games[free_id] = new_game
     game_id = free_id
     free_id+=1
-    await bot.send_message(player1, 'Игра началась!\nЧтобы завершить игру до её окончания, нажмите /stop ')
-    await bot.send_message(player2, 'Игра началась!\nЧтобы завершить игру до её окончания, нажмите /stop ')
+    await bot.send_message(player1, f"Игра началась!\nВам нужно поставить {games[game_id].need_to_win} {games[game_id].symb[0]} в ряд или в диагональ для победы. \nЧтобы завершить игру до её окончания, нажмите /stop ")
+    await bot.send_message(player2, f"Игра началась!\nВам нужно поставить {games[game_id].need_to_win} {games[game_id].symb[1]} в ряд или в диагональ для победы. \nЧтобы завершить игру до её окончания, нажмите /stop ")
     await send_field(game_id)
 
+async def search_for_opponent(user_id, sz):
+    global free_id
+    if waiting[sz] != -1:
+        await start_game(waiting[sz], user_id, sz)
+        waiting[sz] = -1
+    else:
+        waiting[sz] = user_id
 
-@dp.message_handler(commands=['play'])
-async def search_for_opponent(message : types.Message):
-    global waiting, free_id
-    if waiting == message.from_id:
+
+@dp.message_handler(commands=['play'], state=None)
+async def go_choose_sz(message : types.Message):
+    check_auth(message.from_id)
+    game_id = get_user_game(message.from_id)
+    if game_id != -1:
+        await message.reply('Вы сейчас находитесь в игре. Продолжите игру, либо, если хотите её завершить, нажмите /stop ')
+        return
+    if message.from_id in waiting:
         await message.reply('Вы уже в очереди.\nЧтобы остановить поиск, нажмите /cancel ')
         return
-    game_id = get_user_game(message.from_id)
-    if game_id == -1 or game_id >= free_id:
-        await message.reply('Ищу соперника!\nЧтобы остановить поиск, нажмите /cancel ')
-        if waiting != -1:
-            await start_game(waiting, message.from_id)
-            waiting = -1
-        else:
-            waiting = message.from_id
-    else:
-        await message.reply('Вы сейчас находитесь в игре. Продолжите игру, либо, если хотите её завершить, нажмите /stop ')
+    await FSM_search_game.sz.set()
+    kb = ReplyKeyboardMarkup(row_width=3).add(KeyboardButton('3x3'), KeyboardButton('5x5'), KeyboardButton('7x7'))
+    await message.reply("Выберите размер игрового поля!", reply_markup=kb)
+
+@dp.message_handler(state=FSM_search_game.sz)
+async def sz_chosen(message: types.Message, state : FSM_search_game):
+    allowed_sz = ('3x3', '5x5', '7x7')
+    if not message.text in allowed_sz:
+        await message.reply('Выберите корректный размер поля из предложенных ниже!')
+        return
+    async with state.proxy() as data:
+        data['sz'] = int(message.text[0])
+
+    async with state.proxy() as data:
+        await message.reply('Ищу соперника!\nЧтобы остановить поиск, нажмите /cancel ', reply_markup=ReplyKeyboardRemove())
+        await search_for_opponent(message.from_id, data['sz'])
+
+    await state.finish()
 
 async def finish_game(game_id, someone=0):
     players = games[game_id].players
@@ -185,6 +223,7 @@ async def finish_game(game_id, someone=0):
 
 @dp.message_handler(commands=['stop'])
 async def stop_game(message : types.Message):
+    check_auth(message.from_id)
     global free_id
     game_id = get_user_game(message.from_id)
     if game_id == -1 or game_id >= free_id:
@@ -193,13 +232,14 @@ async def stop_game(message : types.Message):
     await finish_game(game_id, 1)
 
 @dp.message_handler(commands=['cancel'])
-async def stop_game(message : types.Message):
+async def stop_searching(message : types.Message):
+    check_auth(message.from_id)
     global waiting
     user_id = message.from_id
-    if waiting != user_id:
+    if user_id not in waiting:
         await message.reply("Вы сейчас не в поиске.\nЧтобы начать новую игру, нажмите /play ")
         return
-    waiting = -1
+    waiting[waiting.index(user_id)] = -1
     await message.reply("Поиск остановлен.\nЧтобы начать новую игру, нажмите /play ")
     
 
